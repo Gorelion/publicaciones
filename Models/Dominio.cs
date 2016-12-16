a12ca43940435db3d0b518285d48f4f92090669f
RodrigoPizarro <rpz001@alumnos.ucn.cl>
2016-12-15 23:28:08 -0300

    Clases Indice e ImpactoIndice creadas
    Se crearon las clases Indice e Impacto Indice. Aún falta por terminar
    una parte de ImpactoIndice (agregar el atributo IdRevista que lo
    relaciona con la revista, una vez que se haya subido la clase Revista).

--- Models/Dominio.cs
+++ Models/Dominio.cs
@@ -105,4 +105,65 @@ namespace Publicaciones.Models
         public string IdPersona { get; set; }
 
     }
+
+    ///<summary>
+    /// Clase que se encarga de indexar las revistas.
+    ///</summary>
+    ///<returns></returns>
+    public class Indice
+    {
+
+       ///<summary>
+       ///Identificador del indice.
+       ///</summary>
+       //<returns></returns>
+       public int IdIndice { get; set; }
+
+       ///<summary>
+       ///Nombre del indice.
+       ///</summary>
+       //<returns></returns>
+       public string Nombre { get; set; }
+
+    }
+
+    ///<summary>
+    ///Clase que se encarga de calificar a las revistas segun el indice.
+    ///</summary>
+    ///<returns></returns>
+    public class ImpactoIndice
+    {
+
+       ///<summary>
+       ///Identificador del impacto.
+       ///</summary>
+       //<returns></returns>
+       public int IdImpacto { get; set; }
+
+       ///<summary>
+       ///Grado de impacto que tiene la revista (Q1,Q2,Q3,Q4).
+       ///</summary>
+       //<returns></returns>
+       public string QIndice { get; set; }
+
+       ///<summary>
+       ///Fecha en la que fue indexada la revista.
+       ///</summary>
+       //<returns></returns>
+       public string Fecha { get; set; }
+
+       ///<summary>
+       ///Journalist impact factor.
+       ///</summary>
+       //<returns></returns>
+       public string Jif { get; set; }
+
+       ///<summary>
+       ///Indice al que se le asocia el impacto.
+       ///</summary>
+       //<returns></returns>
+       public int IdIndice { get; set; }
+
+
+    }
 }
